ccaf5b82fe2e6314c362cfbbe57f6a4c4cc7e868
nmcdowell00 <nm10954@gmail.com>
2025-01-23 18:35:28 -0500

    ughh

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,24 +4,24 @@ import { cldAssetsLoader} from "astro-cloudinary/loaders";
 const eventsCollection = defineCollection({
     type: "content",
     schema: z.object({
-        title: z.string(),
+        name: z.string(),
         date: z.string(),
         time: z.string(),
         description: z.string()
 
     }),
 });
-const artisansCollection = defineCollection({
-    type: "content",
-    schema: z.object({
-        name: z.string(),
-        profession: z.string(),
-        image: z.string(),
-    }),
-});
+// const artisansCollection = defineCollection({
+//     type: "content",
+//     schema: z.object({
+//         name: z.string(),
+//         profession: z.string(),
+//         image: z.string(),
+//     }),
+// });
 
 export const collections = {
     events: eventsCollection,
-    artisans: artisansCollection
+    // artisans: artisansCollection
 };
 // 4. Export a single `collections` object to register your collection(s)
